02693b9cec8cae98b1c69c00b2f0c3762f01aa48
Yuchen Wang <yulon@me.com>
2015-07-14 16:03:42 +0800

    Added Statuses.FriendsTimeline & Statuses.HomeTimeline

--- statuses.go
+++ statuses.go
@@ -13,18 +13,34 @@ func NewStatuses(app *App) *Statuses {
 	return &Statuses{app}
 }
 
-func (s *Statuses) PublicTimeline(count int, page int, baseApp bool) []*Status {
+func (s *Statuses) PublicTimeline(count int, page int, baseApp int) []*Status { //返回最新的公共微博
 	p := &url.Values{}
 	p.Set("count", strconv.Itoa(count))
 	p.Set("page", strconv.Itoa(page))
-	ba := 0
-	if baseApp {
-		ba = 1
-	}
-	p.Set("base_app", strconv.Itoa(ba))
-	ret := &struct{
-		Statuses []*Status `json: statuses`
-	}{}
-	s.app.Get("statuses/public_timeline", p, ret)
-	return ret.Statuses
+	p.Set("base_app", strconv.Itoa(baseApp))
+	return s.app.GetStatuses("statuses/public_timeline", p)
+}
+
+func (s *Statuses) FriendsTimeline(sinceId int64, maxId int64, count int, page int, baseApp int, feature int, trimUser int) []*Status { //获取当前登录用户及其所关注用户的最新微博
+	p := &url.Values{}
+	p.Set("since_id", strconv.FormatInt(sinceId, 10))
+	p.Set("max_id", strconv.FormatInt(maxId, 10))
+	p.Set("count", strconv.Itoa(count))
+	p.Set("page", strconv.Itoa(page))
+	p.Set("base_app", strconv.Itoa(baseApp))
+	p.Set("feature", strconv.Itoa(feature))
+	p.Set("trim_user", strconv.Itoa(trimUser))
+	return s.app.GetStatuses("statuses/friends_timeline", p)
+}
+
+func (s *Statuses) HomeTimeline(sinceId int64, maxId int64, count int, page int, baseApp int, feature int, trimUser int) []*Status { //获取当前登录用户及其所关注用户的最新微博
+	p := &url.Values{}
+	p.Set("since_id", strconv.FormatInt(sinceId, 10))
+	p.Set("max_id", strconv.FormatInt(maxId, 10))
+	p.Set("count", strconv.Itoa(count))
+	p.Set("page", strconv.Itoa(page))
+	p.Set("base_app", strconv.Itoa(baseApp))
+	p.Set("feature", strconv.Itoa(feature))
+	p.Set("trim_user", strconv.Itoa(trimUser))
+	return s.app.GetStatuses("statuses/home_timeline", p)
 }
