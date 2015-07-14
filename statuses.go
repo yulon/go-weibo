ac1eb86d3f39d92fc84aefd54891e9f2f583d9ba
Yuchen Wang <yulon@me.com>
2015-07-14 17:21:48 +0800

    Update Statuses read interface

--- statuses.go
+++ statuses.go
@@ -140,3 +140,60 @@ func (s *Statuses) MentionsIds(sinceId int64, maxId int64, count int, page int, 
 	p.Set("filter_by_type", strconv.Itoa(filterByType))
 	return s.app.GetStatusesIds("statuses/mentions/ids", p)
 }
+
+func (s *Statuses) BilateralTimeline(sinceId int64, maxId int64, count int, page int, baseApp int, feature int, trimUser int) []*Status { //获取双向关注用户的最新微博
+	p := &url.Values{}
+	p.Set("since_id", strconv.FormatInt(sinceId, 10))
+	p.Set("max_id", strconv.FormatInt(maxId, 10))
+	p.Set("count", strconv.Itoa(count))
+	p.Set("page", strconv.Itoa(page))
+	p.Set("base_app", strconv.Itoa(baseApp))
+	p.Set("feature", strconv.Itoa(feature))
+	p.Set("trim_user", strconv.Itoa(trimUser))
+	return s.app.GetStatuses("statuses/bilateral_timeline", p)
+}
+
+func (s *Statuses) Show(id int64) *Status { //根据微博ID获取单条微博内容
+	p := &url.Values{}
+	p.Set("id", strconv.FormatInt(id, 10))
+	return s.app.GetStatus("statuses/show", p)
+}
+
+func (s *Statuses) ShowBatch(ids string, trimUser int) []*Status { //【高】根据微博ID获取单条微博内容
+	p := &url.Values{}
+	p.Set("ids", ids)
+	p.Set("trim_user", strconv.Itoa(trimUser))
+	return s.app.GetStatuses("statuses/show_batch", p)
+}
+
+func (s *Statuses) Querymid(id int64, typ int, isBatch int) string { //通过微博（评论、私信）ID获取其MID
+	p := &url.Values{}
+	p.Set("id", strconv.FormatInt(id, 10))
+	p.Set("type", strconv.Itoa(typ))
+	p.Set("is_batch", strconv.Itoa(isBatch))
+	return s.app.GetMid("statuses/querymid", p)
+}
+
+func (s *Statuses) Queryid(mid string, typ int, isBatch int, inbox int, isBase62 int) string { //通过微博（评论、私信）MID获取其ID
+	p := &url.Values{}
+	p.Set("mid", mid)
+	p.Set("type", strconv.Itoa(typ))
+	p.Set("is_batch", strconv.Itoa(isBatch))
+	p.Set("inbox", strconv.Itoa(inbox))
+	p.Set("isBase62", strconv.Itoa(isBase62))
+	return s.app.GetId("statuses/queryid", p)
+}
+
+func (s *Statuses) Count(ids string) (scs []*StatusCount) { //批量获取指定微博的转发数评论数
+	p := &url.Values{}
+	p.Set("ids", ids)
+	s.app.Get("statuses/count", p, scs)
+	return
+}
+
+func (s *Statuses) Go(uid int64, id int64) string { //生成根据ID跳转到单条微博页的URL
+	p := &url.Values{}
+	p.Set("uid", strconv.FormatInt(uid, 10))
+	p.Set("id", strconv.FormatInt(id, 10))
+	return "http://api.weibo.com/2/statuses/go?" + p.Encode()
+}
